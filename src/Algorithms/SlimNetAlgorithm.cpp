5d324747956c736e18e4d8f6c00c6fa12698a1e3
Suraj Ketan Samal <sksamal@gmail.com>
2016-06-13 18:02:19 -0500

    Partial updation to SlimnetAlgorithm

--- src/Algorithms/SlimNetAlgorithm.cpp
+++ src/Algorithms/SlimNetAlgorithm.cpp
@@ -65,6 +65,25 @@ SlimNetAlgorithm::~SlimNetAlgorithm()
 }
 
 //------------------------------------------------------------------------------
+//  Helper menthod to generate Slimnet address 
+//------------------------------------------------------------------------------
+void genSlimNetAddress(int number, int X, int *address, int length) {
+   int i=0;
+   while(number>0)  //number in base 10
+   {
+     address[length-1-i]= (number%X);
+     number = number/X;
+     i++;
+   }
+
+   while(i<length)
+   { 
+     address[length-1-i]=0;
+     i++;
+   }
+}
+
+//------------------------------------------------------------------------------
 //  Path WSSlimNetAlgorithm::compute()
 //------------------------------------------------------------------------------
 Path SlimNetAlgorithm::compute(const int &flow_source,
@@ -81,43 +100,111 @@ Path SlimNetAlgorithm::compute(const int &flow_source,
         double  length;
         bool    visited;
     };
-    NodeLabel* state = new NodeLabel[number_of_nodes];
+
+    struct LevelPath
+    {
+	int level;
+	int source;
+	int destination;
+    };
     IntVector extras = topology->getExtras();
     int k = extras[0];  // No of levels
     int hosts_per_rack = extras[1];  // Hosts per rack 
-    int racks_per_level = extras[2];  // Range of y, No of cells = x*y
-
+    int racks_per_level = extras[2];  // Racks in each level , safely assume 8 
+    int aggs = pow (racks_per_level, k+1);
     int hosts = hosts_per_rack*aggs;
-    int addresses[hosts][k+1];
-    for(int node= host;node < hosts; node++) {
+    int addresses[aggs][k+1];
+    NodeLabel* state = new NodeLabel[aggs];  // Maximum number of nodes possible in path
+    std::deque<LevelPath> pathq;
+
+    // Node has to be one of the TORs and not hosts, if not return null.
+    if(flow_source < hosts || flow_destination < hosts)
+	return result;
+
+    // Generate addresses in slimnet format
+    for(int node= 0;node < aggs; node++) {
           genSlimNetAddress(node,racks_per_level,addresses[node],k+1);
 //        cout<<"node="<<node<<"Address="<<FormattedSlimNetAdd(addresses[node],k+1)<<endl;
        }
 //
-    // Initialization
-    for (int counter = 0; counter < number_of_nodes; ++counter)
+    // Initialization - mark all nodes as not visited
+    for (int counter = 0; counter < aggs; ++counter)
     {
         // CHANGED FROM 0 TO (N+1) BECAUSE TOPOLOGY STARTS COUNTING AT NODE 0
-        state[counter].predecessor = number_of_nodes + 1;
+        state[counter].predecessor = aggs + 1;
         state[counter].length = DBL_MAX;
         state[counter].visited  = false;
     }
-    int worknode = flow_source; // First worknode is the source
-    int dest = flow_destination; // First worknode is the source
-    state[worknode].length = 0.0;
-    state[worknode].visited = true;
-    int worknode = address[flow_source];
-    int dest = address[flow_destination];
+
+    state[src].length = 0.0;
+    state[src].visited = true;
+    LevelPath lpath;
+     // Push source and destination to the queue
+    lpath.source = flow_source - hosts;
+    lpath.destination = flow_destination - hosts;
+    lpath.level = k;
+    pathq.push_back(lpath);
 
     cout<<"\nSlimnet(k,hosts_per_rack,racks_per_level): ("<<k<<","<<hosts_per_rack<<","<<racks_per_level<<") , NumNodes="<<number_of_nodes<<endl;
 
-    updateRecursive(flow_source,flow_destination,k,state);
-    if(!state[flow_destination].visited)
-    {  // Some problem
-	PRINT("Error");
-	return NULL;
-    } // end: while...
+    for (int level = k; level >= 0; level--)
+    {
+ 	while(!pathq.empty() && pathq.front().level==k) 
+       {
+	  LevelPath nextpath = pathq.pop_front();
+	  int src = nextPath.source;
+	  int dest = nextPath.dest;
+	  int i_level=addresses[src][level], j_level=addresses[dest][level];
+	if(i_level == j_level) // No need to do anything, just go to next level
+	    continue;
+	else
+	   {
+	     int i_0 = addresses[source][0]; 
+	     // Calculate Fxy to see if there is a direct link betweek i_level and j_level
+             int Fxy = (i_0==0) ? ((racks_per_level-1)-i_level) : ((i_level|i_0)&((racks_per_level-1)-(i_level&i_0)));
 
+	     // Direct link exists!
+	     if(Fxy == j_level) {
+		int temp = i_0; mul=1;
+		while(int l=1;l<level;l++) {
+		   mul*=racks_per_level;
+		   temp+=(mul*addresses[source][l])
+		}
+		while(int l=level;l<=k;l++) {
+		   mul*=racks_per_level;
+		   temp+=(mul*addresses[dest][l])
+		}
+	        // Create path between src and temp and mark it as visited
+       		state[temp+hosts].predecessor = src + hosts;
+       		state[temp+hosts].length = state[src + hosts].length + 1.0; /* Assume default 1 metric */
+       		state[temp+hosts].visited = true;
+		// Remaining (temp,dest,l-1) - insert to pathq
+	     }
+	     else // Direct link doesnot exist, find temp1 and temp2
+	     {
+             	Fxy = (j_level==0) ? ((racks_per_level-1)-i_level) : ((i_level|j_level)&((racks_per_level-1)-(i_level&j_level)));
+		int temp = Fxy; mul=1;
+		while(int l=1;l<level;l++) {
+		   mul*=racks_per_level;
+		   temp+=(mul*addresses[source][l])
+		}
+		mul*=racks_per_level;
+		int temp1=temp+(mul*addresses[source][l]);
+		int temp2=temp+(mul*addresses[dest][l]);
+		while(int l=level;l<=k;l++) {
+		   mul*=racks_per_level;
+		   temp1+=(mul*addresses[dest][l])
+		   temp2+=(mul*addresses[dest][l])
+		}
+	        // Create path between temp1 and temp2 and mark it as visited
+       		state[temp2+hosts].predecessor = temp1 + hosts;
+       		state[temp2+hosts].length = state[temp1 + hosts].length + 1.0; /* Assume default 1 metric */
+       		state[temp2+hosts].visited = true;
+		// Remaining is (src,temp1,0) and (temp2,dest,l-1) - insert to pathq
+	     }
+ 	}
+    }
+  }
     // path calculation
     if (state[flow_destination].visited) // path was found
     {
@@ -134,22 +221,5 @@ Path SlimNetAlgorithm::compute(const int &flow_source,
 
     TRACE("SlimNetAlgorithm::compute <--");
     return result;
-}
+ }
 
-void updateRecursive(int src, int dest, int l, int *state) 
-  {
-	if(src == dest) break;
-	else
-	{
-	  if(l==0) {  // only one level exists here
-		// If worknode and dest are connected, create the path and return.
-	  }
-	  else {
-	        // Connect using the next node, which has to be connected.  
-  	  }	        
-	}
-       state[destination].predecessor = worknode + tor;
-       state[destination].length = state[worknode+ tor].length + 1.0; /* Assume default 1 metric */
-       state[destination].visited = true;
-       worknode = destination - tor;
-  } 
